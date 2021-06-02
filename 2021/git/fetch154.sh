0179890b38f36bec4c34ff7c059acab3fe1de1db
chenqixu <13509323824@139.com>
2021-06-02 17:00:33 +0800

    expect改成EOF方式，整合到1个脚本里

--- 2021/git/fetch154.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect
-cd /bi/user/cqx/data/git/205/
-spawn git fetch repo154 develop:repo154
-expect "Username"
-send "chenqixu\n"
-expect "Password"
-send "Cqx123!!\n"
-expect eof
--- 2021/git/fetch154AndUpdate205.sh
+++ 2021/git/fetch154AndUpdate205.sh
@@ -7,19 +7,55 @@
 ########################
 #初始化
 #close 205的develop分支到/bi/user/cqx/data/git/205/目录下
-#git clone -b develop http://10.1.8.205:7080/real-time/etl-jstorm.git /bi/user/cqx/data/git/205/
+#git clone -b develop http://10.1.8.205:7080/real-time/etl-jstorm.git /bi/user/cqx/data/git/205/etl-jstorm.git
+#git clone -b develop http://10.1.8.205:7080/real-time/realtime-jstorm.git /bi/user/cqx/data/git/205/realtime-jstorm.git
 
 #添加154的URL作为新远程仓库，命名为repo154
-#cd /bi/user/cqx/data/git/205/
+#cd /bi/user/cqx/data/git/205/etl-jstorm.git
 #git remote add repo154 http://10.1.8.154:7080/BI-BIGDATA/etl-jstorm.git
+#cd /bi/user/cqx/data/git/205/realtime-jstorm.git
+#git remote add repo154 http://10.1.8.154:7080/BI-BIGDATA/realtime-jstorm.git
 ########################
 
+###############################
+##将154的develop分支获取到205并创建分支为repo154
+###############################
+function fetch154() {
+git_name=$1
+expect <<EOF
+cd /bi/user/cqx/data/git/205/${git_name}
+spawn git fetch repo154 develop:repo154
+expect "Username"
+send "chenqixu\n"
+expect "Password"
+send "Cqx123!!\n"
+expect eof
+EOF
+}
+
+###############################
+##推送到远程205仓库
+###############################
+function push205() {
+git_name=$1
+expect <<EOF
+cd /bi/user/cqx/data/git/205/${git_name}
+spawn git push
+expect "Username"
+send "chenqx\n"
+expect "Password"
+send "Cqx123456\n"
+expect eof
+EOF
+}
+
 function fetch154AndUpdate205() {
-cd /bi/user/cqx/data/git/205/
+git_name=$1
+cd /bi/user/cqx/data/git/205/${git_name}
 #切换到需要合并的分支
 git checkout develop
 #将154的develop分支获取到205并创建分支为repo154
-/bi/user/cqx/data/git/fetch154.sh
+fetch154 ${git_name}
 #切换到repo154分支
 git checkout repo154
 #把当前分支变基到develop分支
@@ -29,10 +65,10 @@ git checkout develop
 #合并变基内容
 git merge repo154
 #推送到远程205仓库
-/bi/user/cqx/data/git/push205.sh
+push205 ${git_name}
 #删除repo154分支
 git branch -d repo154
 }
 
-echo "fetch154AndUpdate205"
-fetch154AndUpdate205;
+fetch154AndUpdate205 "etl-jstorm.git"
+fetch154AndUpdate205 "realtime-jstorm.git"
