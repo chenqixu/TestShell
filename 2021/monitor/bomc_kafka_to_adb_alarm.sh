b01c74df05b9712d7d10afcec9660d2c9e0daec8
chenqixu <13509323824@139.com>
2021-08-06 09:14:16 +0800

    监控更新，积压可以区分话题数据量的大小

--- 2021/monitor/bomc_kafka_to_adb_alarm.sh
+++ 2021/monitor/bomc_kafka_to_adb_alarm.sh
@@ -54,7 +54,7 @@ crontab_task=0
 function monitor() {
 ##kafka监控
 sh ${NLTOOL_COMMAND} kafka_group toolconfig/kafka_group.yaml --group_id ADB_TEST > ${LOGS_PATH}/kafka_group.log
-sh ${NLTOOL_COMMAND} kafka_group toolconfig/kafka_group.yaml --group_id adb_single > ${LOGS_PATH}/kafka_group_single.log
+#sh ${NLTOOL_COMMAND} kafka_group toolconfig/kafka_group.yaml --group_id adb_single > ${LOGS_PATH}/kafka_group_single.log
 sh ${NLTOOL_COMMAND} kafka_group toolconfig/kafka_group_10.yaml > ${LOGS_PATH}/kafka_group_single_10.log
 
 ##ogg schema监控
@@ -66,14 +66,10 @@ crontab_task=`cat /tmp/bomc_file/kafkaToAdbCrontabMonitor.txt`
 }
 
 ##多分区
-multiple_topic="FLAT_NMC_CCS_BROADBAND_USER
-FLAT_NMC_CCS_USER
-FLAT_NMC_CCS_ITV_USERS
-NMC_FLAT_B_BRB_CUS_BROADBAND_MOP_MANAGER_R_I_V1
-NMC_FLAT_B_CUS_CUS_NAME_RECORD_INFO_R_I_V1
+multiple_topic="NMC_FLAT_B_BRB_CUS_BROADBAND_MOP_MANAGER_R_I_V1
 NMC_FLAT_B_TRD_UN_COMMON_FLOW_R_I_V1"
 #FLAT_B_BIL_OUTER_PLATFORM_USER_R_I_V1#定时同步
-multiple_cnt_arr=(6 3 1 6 6 6)
+multiple_cnt_arr=(6 6)
 
 ##单分区
 single_topic="NMC_TB_B_RES_CHNL_STORAGE_SALES_R_I_V1
@@ -83,11 +79,22 @@ single_cnt_arr=(1 1 1)
 
 ##单分区新集群
 single_10_topic="USER_ADDITIONAL_INFO
-NMC_CCS_USER_ODFW_EXPORT_INFO"
-single_10_cnt_arr=(1 1)
+NMC_CCS_USER_ODFW_EXPORT_INFO
+NMC_CCS_BROADBAND_USER
+NMC_CCS_ITV_USERS
+NMC_CCS_USER
+NMC_TB_B_CUS_CUS_NAME_RECORD_INFO_R_I_V1
+NMC_TB_B_RES_CHNL_STORAGE_SALES_R_I_V1
+NMC_TB_B_MKT_RES_PIECE_TYPE_GOODS_CONSUME_R_I_V1
+NMC_TB_B_CUS_USER_PRODUCT_R_I_V1"
+single_10_cnt_arr=(1 1 1 1 1 1 1 1 1)
 
 ##积压告警值
 limit=20
+##适用于数据量比较大的话题
+limit_max=500
+##数据量比较大的话题
+limit_max_topic=(NMC_TB_B_CUS_USER_PRODUCT_R_I_V1)
 
 function add_flag() {
 local flag=$1
@@ -131,6 +138,15 @@ local flag_name=$3
 local t
 for t in ${topic}; do
   local lag=`cat ${LOGS_PATH}/${log_name}.log|awk '{if( $1 == v_name) cnt+=$5}END{print cnt}' v_name=${t}`
+  ##数据量比较大的话题
+  if [[ ${limit_max_topic[@]} =~ ${t} ]]; then
+    if [[ ${lag} -gt ${limit_max} ]]; then
+      fun_log 2 "【kafka消费积压】话题：${t} 积压值：${lag} 告警值：${limit_max}"
+      add_flag "${flag_name}"
+    fi
+    continue
+  fi
+  ##数据量比较小的话题
   if [[ ${lag} -gt ${limit} ]]; then
     fun_log 2 "【kafka消费积压】话题：${t} 积压值：${lag} 告警值：${limit}"
     add_flag "${flag_name}"
@@ -190,13 +206,13 @@ fi
 fun_log 0 "--------------------------------开始脚本逻辑处理--------------------------------";
 #monitor
 count "kafka_group" "${multiple_topic}" "${multiple_cnt_arr[*]}" "MULTIPLE_COUNT_IS_ERR"
-count "kafka_group_single" "${single_topic}" "${single_cnt_arr[*]}" "SINGLE_COUNT_IS_ERR"
+#count "kafka_group_single" "${single_topic}" "${single_cnt_arr[*]}" "SINGLE_COUNT_IS_ERR"
 count "kafka_group_single_10" "${single_10_topic}" "${single_10_cnt_arr[*]}" "SINGLE_10_COUNT_IS_ERR"
 isConsumer "kafka_group" "${multiple_topic}" "MULTIPLE_CONSUMER_IS_ERR"
-isConsumer "kafka_group_single" "${single_topic}" "SINGLE_CONSUMER_IS_ERR"
+#isConsumer "kafka_group_single" "${single_topic}" "SINGLE_CONSUMER_IS_ERR"
 isConsumer "kafka_group_single_10" "${single_10_topic}" "SINGLE_10_CONSUMER_IS_ERR"
 lag "kafka_group" "${multiple_topic}" "MULTIPLE_LAG_IS_ERR"
-lag "kafka_group_single" "${single_topic}" "SINGLE_LAG_IS_ERR"
+#lag "kafka_group_single" "${single_topic}" "SINGLE_LAG_IS_ERR"
 lag "kafka_group_single_10" "${single_10_topic}" "SINGLE_10_LAG_IS_ERR"
 ogg_schema "MULTIPLE_OGG_SCHEMA_IS_ERR"
 
